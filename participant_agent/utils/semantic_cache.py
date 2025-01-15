d4183d179aec9398864232379d2b4c9327c11a8b
Matt DelBrocco <mattdelbrocco@gmail.com>
2025-01-15 10:45:05 -0500

    Workshop step 4 implemented

--- participant_agent/utils/semantic_cache.py
+++ participant_agent/utils/semantic_cache.py
@@ -7,8 +7,10 @@ REDIS_URL = os.environ.get("REDIS_URL", "redis://host.docker.internal:6379/0")
 # Semantic cache
 hunting_example = "There's a deer. You're starving. You know what you have to do..."
 
-# TODO: implement semantic cache
-semantic_cache = None
+semantic_cache = SemanticCache(
+    name="oregon_trail_cache",
+    redis_url=REDIS_URL,
+    distance_threshold=0.1
+)
 
-# TODO store appropriate values in cache
-# semantic_cache.store()
+semantic_cache.store(prompt=hunting_example, response="bang")
